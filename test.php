4190190890476c1f9b2eda8c8e175925091434d4
MarkFirman <mark.firman@me.com>
2020-03-07 19:09:16 +0000

    Update test.php
    Updated test file to reflect changes to the dictionary API

--- test.php
+++ test.php
@@ -14,26 +14,30 @@ include_once 'dictionary.class.php';
 $dictionary = new Dictionary("APP ID", "APP KEY", "en-gb");
 
 /* Send new word request to the dictionary */
-$dictionary->queryWord("Bark");
+$dictionary->newDictionaryRequest("Pizza");
 
 /* Set the result to use - some words might have multiple meanings - use this method to switch between different meanings */
 /* This is only required if you want to specify a result set other than the default */
-/* If you specify a result set that does not exist, it will default to 0 */
+/* If you specify a result set that does not exist, it will default back to 0 */
 $dictionary->setResult(0);
 
 /* Get results from dictionary class */
-echo "<h1>Dictionary Class Results - ".$dictionary->getWord()."</h1>";
-echo "<b>Word:</b> ".$dictionary->getWord();
+echo "<h1>Dictionary Class Results - ".$dictionary->word."</h1>";
+echo "<b>Word:</b> ".$dictionary->word;
 echo "<br><b>Definition:</b> ".$dictionary->getDefinition();
 echo "<br><b>Short Definition:</b> ".$dictionary->getShortDefinition();
-echo "<br><b>Example:</b> ".$dictionary->getExample();
+echo "<br><b>Example:</b> ".$dictionary->getExample(0);
+echo "<br><b>Example 2:</b> ".$dictionary->getExample(1);
 echo "<br><b>Lexical:</b> ".$dictionary->getLexical();
 echo "<br><b>Phonetic:</b> ".$dictionary->getPhonetic();
 echo "<br><b>Origin:</b> ".$dictionary->getOrigin();
-echo "<br><b>Language:</b> ".$dictionary->getLanguage();
+echo "<br><b>Language:</b> ".$dictionary->API_LANG;
 echo "<br><b>Audio:</b> <audio controls><source src='".$dictionary->getAudio()."' type='audio/mpeg'>Your browser does not support HTML audio</audio><br>";
 
-/* Displays the current result set */
+/* Displays the current result set and maximum number of result sets */
 echo "<br></br>Using result set: <b>".$dictionary->selected_result."</b>";
+echo "<br></br>Total result sets available from request: <b>".$dictionary->num_returned_results."</b>";
+
+
 
 ?>
